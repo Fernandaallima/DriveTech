cce6aff0bee9ae378670e9ee555b5d4b7d7f33c0
Alexandre Ferreira de Oliveira <alexandre59081606@edu.df.senac.br>
2025-09-26 21:29:07 -0300

    Chore: Subindo estrutura inicial do projeto

---
+++ DriveTech/views/matuencao.py
@@ -0,0 +1,6 @@
+class Manutencao:
+    def __init__(self, data, descricao, valor, mecanico_responsavel):
+        self.data = data
+        self.descricao = descricao
+        self.valor = valor
+        self.mecanico_responsavel = mecanico_responsavel
